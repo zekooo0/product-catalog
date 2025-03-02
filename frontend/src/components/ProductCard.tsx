e2099dbb111df4385faef959bbbcb7131e2510df
Ahmed Zakaria <ahmedzakria102@gmail.com>
2025-03-02 10:15:04 +0200

    add star icon

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -74,8 +74,10 @@ const ProductHeader = ({
       </h2>
     </a>
     <div className="flex items-center gap-1">
-      <span className="font-bold text-lg">{rating}</span>
-      <span className="text-lg">/10</span>
+      <span className="font-bold text-lg">{starIcon}</span>
+      <span className="text-lg">
+        <span className="font-bold">{rating}</span>/10
+      </span>
     </div>
   </div>
 );
@@ -268,3 +270,32 @@ const ProductCard = ({ product, mutateProducts }: ProductCardProps) => {
 };
 
 export default ProductCard;
+
+const starIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+  >
+    <g fill="none">
+      <path
+        fill="url(#fluentColorStar160)"
+        d="M7.194 2.102a.9.9 0 0 1 1.614 0l1.521 3.082l3.401.494a.9.9 0 0 1 .5 1.535l-2.462 2.4l.581 3.387a.9.9 0 0 1-1.306.948L8.001 12.35l-3.042 1.6A.9.9 0 0 1 3.653 13l.58-3.387l-2.46-2.399a.9.9 0 0 1 .499-1.535l3.4-.494z"
+      />
+      <defs>
+        <linearGradient
+          id="fluentColorStar160"
+          x1="14.5"
+          x2="1.125"
+          y1="14.332"
+          y2="1.72"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#ff6f47" />
+          <stop offset="1" stop-color="#ffcd0f" />
+        </linearGradient>
+      </defs>
+    </g>
+  </svg>
+);
