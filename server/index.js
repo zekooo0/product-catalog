1f741068dbe210ff508f028b473a00baaf28ed38
Ahmed Zakaria <ahmedzakria102@gmail.com>
2025-03-10 22:04:24 +0200

    fix CORS issue

--- server/index.js
+++ server/index.js
@@ -10,7 +10,18 @@ const app = express();
 
 // Middleware
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://product-catalog-frontend.vercel.app",
+      "http://affiliatelist.site",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
@@ -29,7 +40,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
