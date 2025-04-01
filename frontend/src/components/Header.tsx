9cf71409fd93fba6bd691a866b24f2bd01a9222f
Ahmed Zakaria <ahmedzakria102@gmail.com>
2025-04-01 14:52:47 +0200

    update package dependencies, enhance Header component with a download link for a guide, and prevent right-click context menu in ProductsPage

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -22,12 +22,42 @@ const Header = ({
 
   return (
     <div className="flex flex-col gap-5 p-5 sticky top-0 z-50 bg-background">
-      <div className="flex items-center justify-between ">
-        <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
-          <Link href="/" className="text-blue-500">
-            AffiliateList.Site
-          </Link>
-        </h1>
+      <div className="flex items-start justify-between ">
+        <div className="flex flex-col gap-2">
+          <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl">
+            <Link href="/" className="text-blue-500">
+              AffiliateList.Site
+            </Link>
+          </h1>
+          <div className="mt-2 ">
+            <a
+              href="/Comprehensive Guide To Affiliate Programs.pdf"
+              download="Comprehensive Guide To Affiliate Programs.pdf"
+              className="flex items-center justify-center gap-2 text-red-500 hover:underline break-words max-w-xs  border border-dashed p-2 border-red-500 rounded-md"
+            >
+              <div className="bg-red-500 rounded-full p-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="white"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                  <polyline points="7 10 12 15 17 10"></polyline>
+                  <line x1="12" y1="15" x2="12" y2="3"></line>
+                </svg>
+              </div>
+              <span className="font-medium text-center">
+                Free Comprehensive Guide To Affiliate Marketing
+              </span>
+            </a>
+          </div>
+        </div>
         <div className="flex items-center gap-2">
           <ModeToggle />
           {isAuthenticated ? (
--- frontend/src/components/ProductsPage.tsx
+++ frontend/src/components/ProductsPage.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Products from "@/components/Products";
 import Sidebar from "@/components/Sidebar";
 import { useProducts } from "@/hooks/useProducts";
+import { useEffect } from "react";
 
 export default function ProductsPage() {
   const {
@@ -15,6 +16,25 @@ export default function ProductsPage() {
     setSelectedCategory,
     setSelectedLetter,
   } = useProducts();
+
+  useEffect(() => {
+    // define a custom handler function
+    // for the contextmenu event
+    const handleContextMenu = (e: MouseEvent) => {
+      // prevent the right-click menu from appearing
+      e.preventDefault();
+    };
+
+    // attach the event listener to
+    // the document object
+    document.addEventListener("contextmenu", handleContextMenu);
+
+    // clean up the event listener when
+    // the component unmounts
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
 
   return (
     <div className="relative h-screen flex flex-col overflow-hidden">
