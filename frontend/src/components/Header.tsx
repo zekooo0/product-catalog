8990970a9dfd1b7fe71989554cf609e4ab620a50
zekooo0 <ahmedzakria102@gmail.com>
2025-06-08 21:46:19 +0300

    update image size

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = ({
           </h1>
           <Guide />
         </div>
-        <div className="relative h-full w-[60%] px-10">
+        <div className="relative h-full w-1/2 px-10">
           <Image
             src={"/banner.png"}
             fill
